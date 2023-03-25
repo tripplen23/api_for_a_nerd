b911cec0b9c9908b6faf6c705cc0a74e385c028d
tripplen23 <tripplen63@gmail.com>
2023-03-25 10:51:51 +0700

    Update: JWT, user model and user resource

--- python-for-beginners/first_rest_api/app.py
+++ python-for-beginners/first_rest_api/app.py
@@ -1,7 +1,9 @@
 import os
+import secrets
 
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
 import models
@@ -20,15 +22,23 @@ def create_app(db_url=None):
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/" # To tell flask smorest to where the route the API is
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui" # To tell flask smorest to where the route the API is
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/" #To tell flask smorest to where the route the API is
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
+
     api = Api(app)
 
-    with app.app_context():
+    #Set a secret key , And the secret key for signing the JWT secrets.SystemRandom().getrandbits(128)
+    app.config["JWT_SECRET_KEY"] = "Ben"
+    # Create an instance of the JWT manager
+    jwt = JWTManager(app)
+
+    @app.before_first_request 
+    def create_tables():
         db.create_all()
+        
 
     # blp variable in the resources file
     api.register_blueprint(ItemBlueprint)
